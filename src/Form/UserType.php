dd4bcb5648b8b0afb3ea11d34fd322310b260e28
EdouardH <ehsurfdkr@hotmail.com>
2023-07-19 22:34:15 +0200

    made cookies, legal notice, privacy policy pages

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,19 +4,47 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('dateOfBirth')
-
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'attr' => ['placeholder' => 'Entrez votre adresse e-mail'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre adresse e-mail.']),
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['placeholder' => 'Entrez votre prénom'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre prénom.']),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Entrez votre nom'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre nom.']),
+                ],
+            ])
+            ->add('dateOfBirth', DateType::class, [
+                'label' => 'Date de naissance',
+                'widget' => 'single_text',
+                'attr' => ['placeholder' => 'Sélectionnez votre date de naissance'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez sélectionner votre date de naissance.']),
+                ],
+            ])
         ;
     }
 
